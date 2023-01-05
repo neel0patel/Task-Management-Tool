5eafb08f1568fb848c57d1bb2256740db4b3a5b4
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2023-01-05 18:54:57 -0500

    adding the methods to the endpoints

--- routes/todo.js
+++ routes/todo.js
@@ -2,37 +2,45 @@
 const express = require("express");
 const router = express.Router();
 
-// GET method to read all the task
+// importing methods for CRUD operations
+const {
+  getAllTodo,
+  postCreateTodo,
+  putUpdateTodo,
+  deleteTodo,
+} = require("../controllers/todo");
+
+// GET method to read all the todo's
 /**
  * @route GET api/todo
  * @description get all todo
  * @access public
  */
-router.get("/");
+router.get("/", getAllTodo); // added methods to end points
 
-// POST method to create a new task
+// POST method to create a new todo
 /**
  * @route POST api/todo
  * @description get all todo
  * @access public
  */
-router.post("/");
+router.post("/", postCreateTodo); // added methods to end points
 
-// PUT method to update a exisiting task
+// PUT method to update a exisiting todo
 /**
  * @route PUT api/todo/:id
  * @description update a todo
  * @access public
  */
-router.put("/:id");
+router.put("/:id", putUpdateTodo); // added methods to end points
 
-// DELETE method to delete a exisiting task
+// DELETE method to delete a exisiting todo
 /**
  * @route DELETE api/todo/:id
  * @description delete todo
  * @access public
  */
-router.delete("/:id");
+router.delete("/:id", deleteTodo); // added methods to end points
 
 // export router
 module.exports = router;
