a43355fc98827ec6ef11d37aaa147cd4f1b5afaf
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2023-01-05 19:42:56 -0500

    imported routes to the endpoints/ added cors

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-// import and initializing express and dotenv
+// import and initializing express, dotenv and cors
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 // import connectDB method from config/db.js
 const connectDB = require("./config/db");
 
 const app = express();
 
+// import routes
+const todo = require("./routes/todo");
+
 // call the method
 connectDB();
+
+// initializing cors to be used
+app.use(cors({ origin: true, credentials: true }));
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running!"));
+
+// use the imported routes
+app.use("/api/todo", todo);
 
 // setting up port
 const PORT = process.env.PORT || 3000;
